61ee16636ba0c976820c7b424b146fc09650b85b
Umair Ashraf <umrashrf@gmail.com>
2026-04-17 18:46:50 -0600

    Force IPv4

--- riamumail/src/riamumail/app.py
+++ riamumail/src/riamumail/app.py
@@ -646,6 +646,14 @@ class SetupApp(toga.App):
     # ------------------ HELPERS ------------------
 
     def get_public_ip(self):
+        # Force socket to use IPv4 only
+        orig_getaddrinfo = socket.getaddrinfo
+
+        def getaddrinfo_ipv4(host, port, family=0, type=0, proto=0, flags=0):
+            return orig_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
+
+        socket.getaddrinfo = getaddrinfo_ipv4
+
         try:
             return requests.get("https://ipecho.net/plain", timeout=5).text.strip()
         except Exception:
