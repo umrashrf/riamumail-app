e89b7d65872f2d18f109d613ceb74207c6ade3ab
Umair Ashraf <umrashrf@gmail.com>
2026-05-10 14:38:32 -0600

    Add pyproject.toml for end users

---
+++ riamumail/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+Setup script for Riamu Mail.
+
+This file exists for backwards compatibility with older pip versions
+and legacy build systems. Modern installations should use pyproject.toml.
+"""
+
+from setuptools import setup
+
+# All configuration is in pyproject.toml
+# This file is only needed for backwards compatibility
+setup()
